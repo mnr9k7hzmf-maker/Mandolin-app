7167bf48b7f484d29ef6fe40a9f82bc7d8b681e4
mnr9k7hzmf-maker <mnr9k7hzmf@privaterelay.appleid.com>
2026-01-08 15:00:21 -0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,3 @@
-const CACHE = "mandolin-v1";
-const FILES = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/pitch.js"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
